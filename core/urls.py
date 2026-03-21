8e0065df906eb95e5f8485909c57f125cbf258e2
NoOneForever <152205739+NepTuneForever@users.noreply.github.com>
2026-03-21 15:43:20 -0300

    CRUD Completo + Opção de comparar valores.

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import CryptoListView, VerFavoritos
+from .views import CryptoListView, VerFavoritos, AnalisarPrecos, AtualizarPrecos
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', CryptoListView.as_view(), name='cripto-list'),
     path('favoritos/', VerFavoritos.as_view(), name='ver-favoritos'),
     path('login/', obtain_auth_token),
+    path('analises/', AnalisarPrecos.as_view(), name='analisar-precos'),
+    path('atualizar/<int:pk>/', AtualizarPrecos.as_view(), name='atualizar-precos'),
 ]
 
 # Superuser criado para acessar a API de autenticação do Django rest framework;
